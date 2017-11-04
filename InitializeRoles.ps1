571ea33d402846535437b1d987628f2b1e9acedc
Ned Bellavance <nbellavance@anexinet.com>
2017-11-04 15:01:20 -0400

    add dismount

--- InitializeRoles.ps1
+++ InitializeRoles.ps1
@@ -11,13 +11,13 @@ function Disable-InternetExplorerESC {
 Disable-InternetExplorerESC
 
 #Initialize all disks
-get-disk | where{$_.PartitionStyle -eq "RAW"} | Initialize-Disk -PartitionStyle GPT
+get-disk | where {$_.PartitionStyle -eq "RAW"} | Initialize-Disk -PartitionStyle GPT
 
 #Extend C drive
 Resize-Partition -DriveLetter C -Size (Get-PartitionSupportedSize -DriveLetter C).sizemax
 
 #Install required features
-Add-WindowsFeature Hyper-V,Failover-Clustering, Web-Server -IncludeManagementTools
+Add-WindowsFeature Hyper-V, Failover-Clustering, Web-Server -IncludeManagementTools
 Add-WindowsFeature RSAT-AD-PowerShell, RSAT-ADDS -IncludeAllSubFeature
 Install-PackageProvider nuget -Force
 
@@ -30,14 +30,13 @@ mkdir C:\ASDK
 Invoke-WebRequest https://aka.ms/azurestackdevkitdownloader -OutFile C:\ASDK\asdk.exe
 $resp = Invoke-RestMethod -Method Get https://aka.ms/azurestack-asdkdownloads
 $resp2 = Invoke-RestMethod -Method Get -Uri $resp.DownloadProduct.DownloadReleases.Uri
-foreach($file in $resp2.DownloadFileList.Files)
-{
+foreach ($file in $resp2.DownloadFileList.Files) {
     Start-BitsTransfer -Source "$($resp2.DownloadFileList.DownloadSource)$($file.FileName)" -Destination "C:\asdk\$($file.FileName)" -Asynchronous
 }
 
-do{
-    $bits = Get-BitsTransfer | ?{$_.jobstate -ne "transferred"}
-}while($bits.count -gt 1)
+do {
+    $bits = Get-BitsTransfer | ? {$_.jobstate -ne "transferred"}
+}while ($bits.count -gt 1)
 
 Get-BitsTransfer | Complete-BitsTransfer
 
@@ -45,20 +44,22 @@ cd C:\ASDK
 
 .\AzureStackDevelopmentKit.exe /verysilent
 
-do{
+do {
     $process = Get-Process -Name AzureStackDevelopmentKit -ErrorAction SilentlyContinue
     Wait-Event -Timeout 5
-}while($process -ne $null)
+}while ($process -ne $null)
 
 
 $vhdxPath = "C:\ASDK\Azure Stack Development Kit\CloudBuilder.vhdx"
 Mount-VHD -Path $vhdxPath -Passthru -ov mount
-$cloudbuilder = Get-Partition | ?{$_.DiskNumber -eq $mount.DiskNumber -and $_.size -gt 1GB}
+$cloudbuilder = Get-Partition | ? {$_.DiskNumber -eq $mount.DiskNumber -and $_.size -gt 1GB}
 #CloudDeployment, fwupdate and tools
 copy-item -path "$($cloudbuilder.DriveLetter):\CloudDeployment" -Destination "C:\" -Recurse
 copy-item -path "$($cloudbuilder.DriveLetter):\fwupdate" -Destination "C:\" -Recurse
 copy-item -path "$($cloudbuilder.DriveLetter):\tools" -Destination "C:\" -Recurse
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/ned1313/AzureStackonAzure/master/PostDeploy.ps1 -OutFile C:\ASDK\PostDeploy.ps1
+
+dismount-vhd -DiskNumber $mount.DiskNumber
 
 Restart-Computer -Force
